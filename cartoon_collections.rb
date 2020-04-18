7eab0921a8c528cd6e23ad8a43353a53ae6b9db5
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-18 18:13:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,9 @@ roll_call_dwarves(dwarves)
 
 planteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
+  
 end
 
 def long_planeteer_calls# code an argument here
