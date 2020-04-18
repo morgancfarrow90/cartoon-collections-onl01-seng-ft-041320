74d038f82ef76f0b05c0035cbf8d287321ec2ef3
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-18 17:49:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)
-  ["Doc", "Dopey", "Bashful", "Grumpy"].each_with_index {|index, name| array[name] = index}
+  ["Doc", "Dopey", "Bashful", "Grumpy"].each_with_index {|name, index| puts "#{index}. #{name}"}
 end
 
 def summon_captain_planet# code an argument here
