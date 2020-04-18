0c7b7b69d3b52ca499f6f5a7c68c995c23cd69e3
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-18 17:59:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,6 @@
 def roll_call_dwarves(array)
   ["Doc", "Dopey", "Bashful", "Grumpy"].each_with_index
-  {|name, index| 
- 
-  puts "#{indexplusone}. #{name}"}
+  {|name, index| puts "#{index}. #{name}"}
   
 end
 
